05bdac4dfe84fec108af0a1b572aea9dce1c5514
M HAMZA SALEEM <autovitytech@gmail.com>
2026-06-02 16:34:22 +0500

    fix: update app (BROKEN VERSION)

--- app/apphealthy.py
+++ app/apphealthy.py
@@ -12,12 +12,16 @@ redis_client = redis.Redis(
 
 @app.route('/')
 def home():
-    # Intentional bug — will crash
-    return 1 / 0  # Division by zero
+    count = redis_client.incr('hits')
+    return {
+        "message": "DevOps Stage 2 - Rolling Update!",
+        "version": "v2",
+        "hit_count": count
+    }
 
 @app.route('/health')
 def health():
-    return {"status": "broken"}
+    return {"status": "healthy", "version": "v2"}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
